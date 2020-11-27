a0a47dcdd757c9f154a6751aa65e6e9c8a6ac977
savisch <savisch@gmail.com>
2020-11-27 13:44:17 -0500

    Set variables in idCard fxn

---
+++ script.js
@@ -0,0 +1,7 @@
+function idCard(){
+    let firstName = document.getElementById("firstName").value
+    let lastName = document.getElementById("lastName").value
+    let age = document.getElementById("age").value
+    let phoneNumber = document.getElementById("phoneNumber").value 
+    let address = document.getElementById("address").value 
+}
