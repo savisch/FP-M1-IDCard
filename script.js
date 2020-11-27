0e52eb01064f24f891518a781c43a64c963b8cf0
savisch <savisch@gmail.com>
2020-11-27 14:31:21 -0500

    Set id Card 'innerHTML' to input values with fxn for age and phone number

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 function idCard(){
-    let firstName = document.getElementById("firstName").value
-    let lastName = document.getElementById("lastName").value
-    let age = document.getElementById("age").value
-    let phoneNumber = document.getElementById("phoneNumber").value 
-    let address = document.getElementById("address").value 
+    let firstName = document.getElementById("firstName").value;
+    let lastName = document.getElementById("lastName").value;
+    let age = document.getElementById("age").value;
+    let phoneNumber = document.getElementById("phoneNumber").value; 
+    let address = document.getElementById("address").value; 
+
+    document.getElementById("postFullName").innerHTML = firstName + " " + lastName;
+    document.getElementById("postAddress").innerHTML = address;
+
+    let numberArray = [];
+
+    numberArray.push(age);
+    numberArray.push(phoneNumber);
+
+    for(let i = 0; i < numberArray.length; i ++){
+        if(numberArray[i] <= 117){
+            document.getElementById("postAge").innerHTML = numberArray[i];
+        }
+        else{
+            document.getElementById("postPhoneNumber").innerHTML = numberArray[i];
+        }
+    }
 }
